1a94217682bdb7fd66f7abdca1750cb069ea80a9
Andrey Kudakov <kudakovandrey@gmail.com>
2017-09-07 23:03:30 +0300

    07.09.2017 23:03

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,7 +1,7 @@
 
 <?php
 
-echo $libs->addLibs(array("jquery 3", "jquery 2", "bootstrap 4 js", "bootstrap 4 css"));
+
 /*echo json_encode($table->result());*/
 
 ?>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php echo $title; ?></title>
+    <?php echo $libs->addLibs(array("jquery 3", "jquery 2", "popper 1", "bootstrap 4 js", "bootstrap 4 css")); ?>
 	<link rel="stylesheet" href="sources/css/style.css">
 </head>
 <body>
